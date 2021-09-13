f0eeb9714abea1c3d7231bea0613c33092475d1d
pragatirahul123 <47949770+pragatirahul123@users.noreply.github.com>
2021-09-13 15:57:23 +0530

    Add files via upload

---
+++ lower_upper.js
@@ -0,0 +1,41 @@
+// var a = ["pragati"]
+// var b = a.map(a => a.toUpperCase())
+// console.log(b)
+
+
+
+// var a = ["pragati"]
+// for(i in a){
+//     if (i.toLowerCase()){
+//         console.log(a[i].toUpperCase())
+//     }else{
+//         console.log("nthing")
+//     }
+
+// }
+
+// var a = [1,2,3,4,1,3]
+// var arr =[]
+// for(var i = 0;i< a.length;i++){
+//     for(var j=i+1;j<a.length;j++){
+//         if(a[i] == a[j]){
+//             arr.push((a[i]))
+
+//         }
+//     }
+// }
+// console.log(arr)
+
+
+// var empty=[]
+// var a = ["pragati"]
+// for(i in a){
+//     if (i.toLowerCase()){
+//         empty.push(a[i].toUpperCase())
+//         console.log(empty)
+//     }else{
+//         console.log("nothing")
+//     }
+
+// }
+
